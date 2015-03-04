a13f7b6136a2979f74e3012a972a29ab87e848e1
lp10833 <kim.jans@linecorp.com>
2015-03-04 18:12:37 +0900

    view Pager 샘플 추가.

--- src/com/example/stest/MainActivity.java
+++ src/com/example/stest/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.stest;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.FragmentManager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
+	
+	private int NUM_PAGES =3;
+	
+	ViewPager mViewPager;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mViewPager = (ViewPager) findViewById(R.id.ViewPager_information_content_view);
+		mViewPager.setAdapter(    new pagerAdapter(    getSupportFragmentManager()   )      );
+		
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -30,5 +43,39 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	public final static int FRAGMENT_PAGE1 = 0;
+	public final static int FRAGMENT_PAGE2 =1;
+	public final static int FRAGMENT_PAGE3 = 2;
+	
+	private class pagerAdapter extends FragmentPagerAdapter{
+		
+		pagerAdapter( android.support.v4.app.FragmentManager fm){
+			super(fm);
+		}
+		@Override
+		public Fragment getItem(int position){
+			
+			switch(position){
+				case FRAGMENT_PAGE1 :
+						return new page1Activity();
+				case FRAGMENT_PAGE2 :
+					return new page2Activity();
+				case FRAGMENT_PAGE3 :
+					return new page3Activity();
+				default :
+						return null;
+			}
+			
+		}
+		
+		@Override
+		public int getCount(){
+			return NUM_PAGES;
+		}
+		
+		
 	}
 }
---
+++ src/com/example/stest/page3Activity.java
@@ -0,0 +1,31 @@
+package com.example.stest;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class page3Activity extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		//return super.onCreateView(inflater, container, savedInstanceState);
+		RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.activity_page3, container, false);
+		return layout;
+		
+	}
+
+	
+}
